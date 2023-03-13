dec4911ecc7d9104e2b212e398e42ea21e86e4ce
Kazi Shihab Jaman <kazishihab7980@gmail.com>
2023-03-13 20:40:40 +0530

    commit 3.0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
+
 import './index.css';
 import App from './App';
+import { ContextProvider } from "./contexts/ContextProvider";
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(
+    <ContextProvider>
+        <App />
+
+    </ContextProvider>,
+document.getElementById('root')
+);
